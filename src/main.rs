11bf6b075557314d43fc7b5f367e926d1f4c4f1b
lthoerner <lowellashton@gmail.com>
2023-09-27 21:09:32 -0700

    feat(customer): add basic data types for customer information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#![allow(dead_code)]
+
+use phonenumber::PhoneNumber;
+
 fn main() {
-    println!("Hello, world!");
+    todo!()
+}
+
+struct PlaceholderType;
+
+struct Customer {
+    name: Name,
+    primary_phone: PhoneNumber,
+    alternate_phones: Vec<PhoneNumber>,
+    email: EmailAddress,
+    contact_method: ContactMethod,
+    // TODO: This will be replaced by a refferal info type later
+    referral: Option<Box<Customer>>,
+}
+
+struct Name {
+    first: String,
+    last: String,
+}
+
+struct EmailAddress {
+    user: String,
+    domain: Domain,
+}
+
+struct Domain {
+    name: String,
+    tld: String,
+}
+
+enum ContactMethod {
+    Call,
+    Text,
+    Email,
+    None,
 }
