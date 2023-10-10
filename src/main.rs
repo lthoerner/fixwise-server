24fd45e819f0bf3ca2dfe09a5876ca810e39621c
lthoerner <lowellashton@gmail.com>
2023-10-10 14:41:03 -0700

    chore(inventory): write doc comments for tests and change some variable names for readability

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ mod tests {
     use crate::extension::{ExtensionManager, InventoryExtension};
 
     #[tokio::test]
+    /// Tests that two extensions with the same ID, but incompatible metadata, will cause an error.
     async fn incompatible_duplicate_extensions() {
         let db = Database::connect_with_name("test_incompatible_duplicate_extensions").await;
 
@@ -39,6 +40,7 @@ mod tests {
     }
 
     #[tokio::test]
+    /// Tests that two extensions with the same ID and metadata will not be reloaded or conflict.
     async fn compatible_duplicate_extensions() {
         let db = Database::connect_with_name("test_compatible_duplicate_extensions").await;
 
@@ -48,111 +50,113 @@ mod tests {
     }
 
     #[tokio::test]
+    /// Tests that an extension will be replaced by an updated version of itself.
     async fn reload_extension_update() {
         let db = Database::connect_with_name("test_reload_extension_update").await;
 
         // Create two extensions with the same ID, but different versions
-        let mut extension_original = InventoryExtension::test(1);
-        let mut extension_updated = InventoryExtension::test(1);
-        extension_updated.version = Version::new(1, 0, 1);
+        let mut original_extension = InventoryExtension::test(1);
+        let mut updated_extension = InventoryExtension::test(1);
+        updated_extension.version = Version::new(1, 0, 1);
         // Add a different manufacturer to each extension
-        let manufacturer_1 = Manufacturer::test(1, &extension_original.id);
-        let manufacturer_2 = Manufacturer::test(2, &extension_updated.id);
-        extension_original
+        let manufacturer_1 = Manufacturer::test(1, &original_extension.id);
+        let manufacturer_2 = Manufacturer::test(2, &updated_extension.id);
+        original_extension
             .manufacturers
             .push(manufacturer_1.clone());
-        extension_updated.manufacturers.push(manufacturer_2.clone());
+        updated_extension.manufacturers.push(manufacturer_2.clone());
         // Add a different classification to each extension
-        let classification_1 = Classification::test(1, &extension_original.id);
-        let classification_2 = Classification::test(2, &extension_updated.id);
-        extension_original
+        let classification_1 = Classification::test(1, &original_extension.id);
+        let classification_2 = Classification::test(2, &updated_extension.id);
+        original_extension
             .classifications
             .push(classification_1.clone());
-        extension_updated
+        updated_extension
             .classifications
             .push(classification_2.clone());
         // Add a different device to each extension
         let device_1 = Device::test(
             1,
-            &extension_original.id,
+            &original_extension.id,
             &manufacturer_1.id,
             &classification_1.id,
         );
         let device_2 = Device::test(
             2,
-            &extension_updated.id,
+            &updated_extension.id,
             &manufacturer_2.id,
             &classification_2.id,
         );
-        extension_original.devices.push(device_1.clone());
-        extension_updated.devices.push(device_2.clone());
+        original_extension.devices.push(device_1.clone());
+        updated_extension.devices.push(device_2.clone());
 
         // Load the first extension into the database
-        let manager = ExtensionManager::with_extensions([extension_original.clone()]);
+        let manager = ExtensionManager::with_extensions([original_extension.clone()]);
         manager.load_extensions(&db).await.unwrap();
         // Make sure the extension was loaded correctly
-        assert!(db.only_contains(&extension_original).await.unwrap());
+        assert!(db.only_contains(&original_extension).await.unwrap());
         // Reload the extension with the updated version, which should unload the original extension
-        let manager = ExtensionManager::with_extensions([extension_updated.clone()]);
+        let manager = ExtensionManager::with_extensions([updated_extension.clone()]);
         manager.load_extensions(&db).await.unwrap();
         // Make sure the original extension was unloaded and the new version was loaded
-        assert!(db.only_contains(&extension_updated).await.unwrap());
+        assert!(db.only_contains(&updated_extension).await.unwrap());
 
         db.teardown().await;
     }
 
     #[tokio::test]
+    /// Tests that an extension will be replaced by the same extension with the load override flag.
     async fn reload_extension_override() {
         let db = Database::connect_with_name("test_reload_extension_override").await;
 
         // Create two extensions with the same metadata, but with a load override
-        let mut extension_original = InventoryExtension::test(1);
-        let mut extension_reloaded = InventoryExtension::test(1);
-        extension_reloaded.load_override = true;
+        let mut original_extension = InventoryExtension::test(1);
+        let mut reloaded_extension = InventoryExtension::test(1);
+        reloaded_extension.load_override = true;
         // Add a different manufacturer to each extension
-        let manufacturer_1 = Manufacturer::test(1, &extension_original.id);
-        let manufacturer_2 = Manufacturer::test(2, &extension_reloaded.id);
-        extension_original
+        let manufacturer_1 = Manufacturer::test(1, &original_extension.id);
+        let manufacturer_2 = Manufacturer::test(2, &reloaded_extension.id);
+        original_extension
             .manufacturers
             .push(manufacturer_1.clone());
-        extension_reloaded
+        reloaded_extension
             .manufacturers
             .push(manufacturer_2.clone());
         // Add a different classification to each extension
-        let classification_1 = Classification::test(1, &extension_original.id);
-        let classification_2 = Classification::test(2, &extension_reloaded.id);
-        extension_original
+        let classification_1 = Classification::test(1, &original_extension.id);
+        let classification_2 = Classification::test(2, &reloaded_extension.id);
+        original_extension
             .classifications
             .push(classification_1.clone());
-        extension_reloaded
+        reloaded_extension
             .classifications
             .push(classification_2.clone());
         // Add a different device to each extension
         let device_1 = Device::test(
             1,
-            &extension_original.id,
+            &original_extension.id,
             &manufacturer_1.id,
             &classification_1.id,
         );
         let device_2 = Device::test(
             2,
-            &extension_reloaded.id,
+            &reloaded_extension.id,
             &manufacturer_2.id,
             &classification_2.id,
         );
-        extension_original.devices.push(device_1.clone());
-        extension_reloaded.devices.push(device_2.clone());
+        original_extension.devices.push(device_1.clone());
+        reloaded_extension.devices.push(device_2.clone());
 
         // Load the first extension into the database
-        let manager = ExtensionManager::with_extensions([extension_original.clone()]);
+        let manager = ExtensionManager::with_extensions([original_extension.clone()]);
         manager.load_extensions(&db).await.unwrap();
         // Make sure the extension was loaded correctly
-        assert!(db.only_contains(&extension_original).await.unwrap());
+        assert!(db.only_contains(&original_extension).await.unwrap());
         // Reload the extension, which should unload the original extension
-        let manager = ExtensionManager::with_extensions([extension_reloaded.clone()]);
+        let manager = ExtensionManager::with_extensions([reloaded_extension.clone()]);
         manager.load_extensions(&db).await.unwrap();
         // Make sure the original extension was unloaded and the new version was loaded
-        assert!(db.only_contains(&extension_reloaded).await.unwrap());
+        assert!(db.only_contains(&reloaded_extension).await.unwrap());
 
         db.teardown().await;
     }
