b07ed4711ee1363bfdb0cd63828980a9fa4a3790
lthoerner <lowellashton@gmail.com>
2024-04-02 17:52:02 -0700

    refactor(dev): change from Prettier SQL to SQL Formatter extension

--- database/config.sql
+++ database/config.sql
@@ -2,38 +2,34 @@ DROP SCHEMA IF EXISTS test CASCADE;
 
 CREATE SCHEMA test;
 
-CREATE TABLE
-    test.inventory (
-        sku serial PRIMARY KEY,
-        name text NOT NULL,
-        count integer NOT NULL,
-        cost numeric(1000, 2) NOT NULL,
-        price numeric(1000, 2) NOT NULL
-    );
+CREATE TABLE test.inventory (
+    sku serial PRIMARY KEY,
+    name text NOT NULL,
+    count integer NOT NULL,
+    cost numeric(1000, 2) NOT NULL,
+    price numeric(1000, 2) NOT NULL
+);
 
-CREATE TABLE
-    test.customers (
-        id serial PRIMARY KEY,
-        name text NOT NULL,
-        email text NOT NULL,
-        phone text NOT NULL,
-        address text
-    );
+CREATE TABLE test.customers (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    email text NOT NULL,
+    phone text NOT NULL,
+    address text
+);
 
-CREATE TABLE
-    test.tickets (
-        id serial PRIMARY KEY,
-        customer_id integer references test.customers (id) NOT NULL,
-        device text NOT NULL,
-        diagnostic text NOT NULL,
-        invoice_amount numeric(1000, 2) NOT NULL DEFAULT 0,
-        payment_amount numeric(1000, 2) NOT NULL DEFAULT 0,
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE test.tickets (
+    id serial PRIMARY KEY,
+    customer_id integer references test.customers (id) NOT NULL,
+    device text NOT NULL,
+    diagnostic text NOT NULL,
+    invoice_amount numeric(1000, 2) NOT NULL DEFAULT 0,
+    payment_amount numeric(1000, 2) NOT NULL DEFAULT 0,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE VIEW
-    test.inventory_view AS
+CREATE VIEW test.inventory_view AS
 SELECT
     sku,
     name,
@@ -45,8 +41,7 @@ FROM
 ORDER BY
     sku ASC;
 
-CREATE VIEW
-    test.customers_view AS
+CREATE VIEW test.customers_view AS
 SELECT
     id,
     name,
@@ -58,8 +53,7 @@ FROM
 ORDER BY
     id ASC;
 
-CREATE VIEW
-    test.tickets_view AS
+CREATE VIEW test.tickets_view AS
 SELECT
     ticket.id,
     customer.name AS customer_name,
